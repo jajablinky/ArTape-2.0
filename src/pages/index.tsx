d7ad690b8f3c910afdf7f737b9d760d89462329e
jajablinky <gmillar6@gmail.com>
2023-03-15 22:43:32 -0700

    functional unordered list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,64 @@
 import Head from "next/head";
 
 import styles from "@/styles/Home.module.css";
-import AudioPlayer from "@/components/AudioPlayer";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+interface Song {
+  title: string;
+  src: string;
+}
+
+const songs: Song[] = [
+  {
+    title: "Blue Cowboy",
+    src: "https://5jxkaucpt4jq2n5ceypa7tijr2vrqcrcgx567hisii27hymlqz2a.arweave.net/6m6gUE-fEw03oiYeD80JjqsYCiI1---dEkI18-GLhnQ",
+  },
+  {
+    title: "Dearly Departed",
+    src: "https://cxdvxsxj6ps45rqsdjnskvgl2n3cc6bvohej5l27w7oof76rhcya.arweave.net/Fcdbyunz5c7GEhpbJVTL03YheDVxyJ6vX7fc4v_ROLA",
+  },
+  {
+    title: "Helmut Lang",
+    src: "https://4hyskfaoe726dpdaczx3dmpswrqj4uydukvzozsuewmcan6wsovq.arweave.net/4fElFA4n9eG8YBZvsbHytGCeUwOiq5dmVCWYIDfWk6s",
+  },
+];
 
 export default function Home() {
-  const [isPlaying, setisPlaying] = useState<number>(0);
-  const audioFiles = [
-    {
-      index: 0,
-      songTitle: "Blue Cowboy",
-      audioLink:
-        "https://5jxkaucpt4jq2n5ceypa7tijr2vrqcrcgx567hisii27hymlqz2a.arweave.net/6m6gUE-fEw03oiYeD80JjqsYCiI1---dEkI18-GLhnQ",
-    },
-    {
-      index: 1,
-      songTitle: "Dearly Departed",
-      audioLink:
-        "https://cxdvxsxj6ps45rqsdjnskvgl2n3cc6bvohej5l27w7oof76rhcya.arweave.net/Fcdbyunz5c7GEhpbJVTL03YheDVxyJ6vX7fc4v_ROLA",
-    },
-    {
-      index: 2,
-      songTitle: "Helmut Lang",
-      audioLink:
-        "https://4hyskfaoe726dpdaczx3dmpswrqj4uydukvzozsuewmcan6wsovq.arweave.net/4fElFA4n9eG8YBZvsbHytGCeUwOiq5dmVCWYIDfWk6s",
-    },
-  ];
-  const audioListLength = audioFiles.length;
+  const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
+  const [isPlaying, setisPlaying] = useState<boolean>(false);
+  const audioPlayer = useRef<HTMLAudioElement>(new Audio());
+
+  useEffect(() => {
+    audioPlayer.current.src = songs[currentSongIndex].src;
+    if (isPlaying) {
+      audioPlayer.current.play();
+    } else {
+      audioPlayer.current.pause();
+    }
+  }, [currentSongIndex, isPlaying]);
+
+  const handlePlayPause = (): void => {
+    setisPlaying(!isPlaying);
+  };
+
+  const handleNextSong = (): void => {
+    setCurrentSongIndex(
+      currentSongIndex === songs.length - 1 ? 0 : currentSongIndex + 1
+    );
+  };
+
+  const handlePrevSong = (): void => {
+    setCurrentSongIndex(currentSongIndex === 0 ? 0 : currentSongIndex - 1);
+  };
+
+  const handleEnded = (): void => {
+    handleNextSong();
+  };
+
+  const handleTrackSelect = (index: number) => {
+    setCurrentSongIndex(index);
+    setisPlaying(true);
+  };
 
   return (
     <>
@@ -40,35 +72,17 @@ export default function Home() {
         <div className={styles.musicPlayerContainer}>
           <div className={styles.musicPlayerHeader}>
             <h2 className={styles.profileName}>SO LOKI</h2>
-            <p className={styles.amountSongTotalDuration}>
-              3 songs, 4:37 minutes
-            </p>
+            <p className={styles.amounttotalDuration}>3 songs, 4:37 minutes</p>
             <p className={styles.artapeLink}>Watch Me Blue</p>
           </div>
-          <AudioPlayer
-            trackNumber={audioFiles[0].index}
-            isPlaying={isPlaying}
-            setisPlaying={setisPlaying}
-            audioLink={audioFiles[0].audioLink}
-            songTitle={audioFiles[0].songTitle}
-            audioListLength={audioListLength}
-          />
-          <AudioPlayer
-            trackNumber={audioFiles[0].index}
-            isPlaying={isPlaying}
-            setisPlaying={setisPlaying}
-            audioLink={audioFiles[1].audioLink}
-            songTitle={audioFiles[1].songTitle}
-            audioListLength={audioListLength}
-          />
-          <AudioPlayer
-            trackNumber={audioFiles[0].index}
-            isPlaying={isPlaying}
-            setisPlaying={setisPlaying}
-            audioLink={audioFiles[2].audioLink}
-            songTitle={audioFiles[2].songTitle}
-            audioListLength={audioListLength}
-          />
+          <audio onEnded={handleEnded} ref={audioPlayer} />
+          <ul>
+            {songs.map((song: Song, index: number) => (
+              <li key={index} onClick={() => handleTrackSelect(index)}>
+                {song.title}
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
