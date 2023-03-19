54da817b79ac24cf4ddbfe51faa949a3f6c4837e
jajablinky <gmillar6@gmail.com>
2023-03-19 14:05:06 -0700

    audio player structured into a component and reuse

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
+import AudioPlayer from "@/components/AudioPlayer";
 
 const profile = () => {
   return (
     <main>
       <div>
         <h1>So Loki</h1>
+        <p>Vancouver, Canada</p>
       </div>
       <div className={styles.gridProfile}>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
-        <div className={styles.profileModule}></div>
+        <div className={styles.profileModule}>
+          <AudioPlayer />
+        </div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
+        <div className={styles.profileModule}>photos</div>
       </div>
     </main>
   );
