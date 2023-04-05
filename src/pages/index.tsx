bc8b4668df2026e3894a65e451e01180ed57bdbd
jajablinky <gmillar6@gmail.com>
2023-04-04 23:31:56 -0700

    styling completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
 import Modal from '@/components/Modal';
+import ArTapeLogo from '../../public/ArTAPE.svg';
+import CassetteLogo from '../../public/Artape-Cassete-Logo.gif';
 
 export default function Home() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -21,7 +23,52 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>new index</p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '64px',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: '24px',
+            }}
+          >
+            <Image src={CassetteLogo} width={25} alt="artape-logo" />
+            <Image src={ArTapeLogo} width={300} alt="artape-logo" />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '14px',
+            }}
+          >
+            <button
+              style={{
+                backgroundColor: 'white',
+                color: 'black',
+                fontSize: '12px',
+              }}
+            >
+              Enter In Vault Number
+            </button>
+            <button
+              style={{
+                background: 'transparent',
+                color: '#ABABAB',
+                fontSize: '12px',
+              }}
+            >
+              Create A New Vault
+            </button>
+          </div>
+        </div>
       </main>
     </>
   );
