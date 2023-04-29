e321625723b6ab511dcd695b0ce02bdc9a1aedc8
jajablinky <gmillar6@gmail.com>
2023-04-28 22:21:40 -0700

    added reorder

--- src/pages/tape/[id].tsx
+++ src/pages/tape/[id].tsx
@@ -1,8 +1,10 @@
 import { useRouter } from 'next/router';
+import { Reorder } from 'framer-motion';
+
 import { useTape } from '@/components/TapeContext';
 import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AudioPlayer, { AudioFile } from '@/components/AudioPlayer';
 import PineappleMemento from '@/components/Images/Mementos/PineappleMemento';
 import LoudMemento from '@/components/Images/Mementos/LoudMemento';
@@ -11,12 +13,13 @@ import CassetteMemento from '@/components/Images/Mementos/CassetteMemento';
 import EditButton from '@/components/Images/UI/EditButton';
 
 const Tape = () => {
+  const [imageFileOrder, setImageFileOrder] = useState<number[]>([]);
+
   const router = useRouter();
   const { id } = router.query;
   const { tape } = useTape();
 
   if (!tape) {
-    // Handle the case when there is no tape data available
     return <div>No tape data available</div>;
   }
   const mementoGenerator = () => {
@@ -33,6 +36,14 @@ const Tape = () => {
     }
   };
   const { audioFiles, tapeInfoJSON, imageFiles } = tape;
+
+  useEffect(() => {
+    if (imageFiles && imageFiles.length > 0) {
+      const order = imageFiles.map((_, index) => index);
+      setImageFileOrder(order);
+    }
+  }, [imageFiles]);
+
   return (
     <>
       <main
@@ -104,105 +115,76 @@ const Tape = () => {
           </div>
         </div>
 
-        <div className={styles.gridProfile}>
-          <div className={styles.profileModule}>
-            {imageFiles[0].url && (
-              <Image
-                className={imageFiles[0].name}
-                src={imageFiles[0].url}
-                alt={imageFiles[0].name}
-                height={350}
-                width={350}
-                style={{ objectFit: 'cover' }}
+        <Reorder.Group
+          values={imageFileOrder}
+          onReorder={setImageFileOrder}
+        >
+          <div className={styles.gridProfile}>
+            <div className={styles.profileModule}>
+              {imageFiles[0].url && (
+                <Image
+                  className={imageFiles[0].name}
+                  src={imageFiles[0].url}
+                  alt={imageFiles[0].name}
+                  height={350}
+                  width={350}
+                  style={{ objectFit: 'cover' }}
+                />
+              )}
+            </div>
+            <div
+              className={styles.profileModuleRectangle}
+              style={{
+                backgroundColor: 'var(--artape-primary-color)',
+                overflow: 'auto',
+              }}
+            >
+              <AudioPlayer
+                audioFiles={
+                  audioFiles.filter(
+                    (audioFile) => audioFile.url !== null
+                  ) as AudioFile[]
+                }
+                tapeInfoJSON={tapeInfoJSON}
+                imageFiles={imageFiles}
               />
-            )}
-          </div>
-          <div
-            className={styles.profileModuleRectangle}
-            style={{
-              backgroundColor: 'var(--artape-primary-color)',
-              overflow: 'auto',
-            }}
-          >
-            <AudioPlayer
-              audioFiles={
-                audioFiles.filter(
-                  (audioFile) => audioFile.url !== null
-                ) as AudioFile[]
-              }
-              tapeInfoJSON={tapeInfoJSON}
-              imageFiles={imageFiles}
-            />
-          </div>
-          {imageFiles &&
-            imageFiles
-              .filter((image) => parseInt(image.moduleId) >= 3)
-              .sort(
-                (a, b) => parseInt(a.moduleId) - parseInt(b.moduleId)
-              )
-              .map((image, index) => {
+            </div>
+            {imageFiles &&
+              imageFileOrder.map((orderIndex) => {
+                const image = imageFiles[orderIndex];
                 if (image.url) {
                   return parseInt(image.moduleId) === 6 ? (
-                    <div
-                      key={image.name}
-                      className={styles.profileModuleRectangle}
-                    >
-                      <Image
-                        className={image.name}
-                        src={image.url}
-                        alt={image.name}
-                        height={350}
-                        width={350}
-                        style={{ objectFit: 'cover' }}
-                      />
-                    </div>
+                    <Reorder.Item key={image.name} value={orderIndex}>
+                      <div className={styles.profileModuleRectangle}>
+                        <Image
+                          className={image.name}
+                          src={image.url}
+                          alt={image.name}
+                          height={350}
+                          width={350}
+                          style={{ objectFit: 'cover' }}
+                        />
+                      </div>
+                    </Reorder.Item>
                   ) : (
-                    <div
-                      key={image.name}
-                      className={styles.profileModule}
-                    >
-                      <Image
-                        className={image.name}
-                        src={image.url}
-                        alt={image.name}
-                        height={350}
-                        width={350}
-                        style={{ objectFit: 'cover' }}
-                      />
-                    </div>
+                    <Reorder.Item key={image.name} value={orderIndex}>
+                      <div className={styles.profileModule}>
+                        <Image
+                          className={image.name}
+                          src={image.url}
+                          alt={image.name}
+                          height={350}
+                          width={350}
+                          style={{ objectFit: 'cover' }}
+                        />
+                      </div>
+                    </Reorder.Item>
                   );
                 }
                 return null;
               })}
-        </div>
-        {/* <div>
-          {audioFiles ? (
-            <>
-              <h1>Audio URLs:</h1>
-              <ul>
-                {audioFiles.map((url, index) => (
-                  <li key={index}>{url}</li>
-                ))}
-              </ul>{' '}
-            </>
-          ) : null}
-          {imageFiles ? (
-            <>
-              <h1>Image URLs:</h1>
-              <ul>
-                {imageFiles.map((url, index) => (
-                  <li key={index}>{url}</li>
-                ))}
-              </ul>{' '}
-            </>
-          ) : null}
-          {tapeInfoJSON ? (
-            <>
-              <h1>Tape Info Metadata:</h1>
-              <p>{JSON.stringify(tapeInfoJSON, null, 2)}</p>
-            </>
-          ) : null}
-        </div> */}
+          </div>
+        </Reorder.Group>
       </main>
     </>
   );
