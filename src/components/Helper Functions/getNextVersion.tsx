024b9b3e156a5034b14c58aeb0899fe6ab131215
jajablinky <gmillar6@gmail.com>
2023-06-18 22:19:59 -0700

    put algo in helper functions

---
+++ src/components/Helper Functions/getNextVersion.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const getNextVersion = (highest: string) => {
+  const [major, minor, lowest] = highest.split('.').map(Number);
+
+  let nextMajor = major;
+  let nextMinor = minor;
+  let nextLowest = lowest + 1;
+
+  if (nextLowest === 10) {
+    nextLowest = 0;
+    nextMinor += 1;
+    if (nextMinor === 10) {
+      nextMinor = 0;
+      nextMajor += 1;
+    }
+  }
+
+  return `${nextMajor}.${nextMinor}.${nextLowest}`;
+};
+
+export default getNextVersion;
--- src/pages/tape/[id]/edit.tsx
+++ src/pages/tape/[id]/edit.tsx
@@ -28,25 +28,7 @@ import EditableAudioPlayer from '@/components/EditableAudioPlayer';
 import Loader from '@/components/Loader';
 import LoadingOverlay from '@/components/LoadingOverlay';
 import AkordSignIn from '@/components/Helper Functions/AkordSignIn';
-
-function getNextVersion(highest: string) {
-  const [major, minor, lowest] = highest.split('.').map(Number);
-
-  let nextMajor = major;
-  let nextMinor = minor;
-  let nextLowest = lowest + 1;
-
-  if (nextLowest === 10) {
-    nextLowest = 0;
-    nextMinor += 1;
-    if (nextMinor === 10) {
-      nextMinor = 0;
-      nextMajor += 1;
-    }
-  }
-
-  return `${nextMajor}.${nextMinor}.${nextLowest}`;
-}
+import getNextVersion from '@/components/Helper Functions/getNextVersion';
 
 const createMetadataJSON = (
   data: VaultValues,
