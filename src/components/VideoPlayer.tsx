a7b583c6881d1c417434fe19e6799971a764b3e2
preston-mattos <preston.mattos@gmail.com>
2023-11-15 11:06:04 -0800

    Added video player control functions

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { VideoFileWithFiles } from '@/types/TapeInfo';
-import {} from './AudioPlayer';
 
 interface VideoPlayerProps {
 	color: string;
@@ -20,8 +19,10 @@ const VideoPlayer = ({color, videoFiles}: VideoPlayerProps) => {
 	// insert react components
 	const videoPlayer = useRef<HTMLVideoElement | null>(null);
 	const [videoFetched, setVideoFetched] = useState<boolean>(true);
+	const [currentVideoIndex, setCurrentVideoIndex] = useState<number>(0);
 	const [isPlaying, setIsPlaying] = useState<boolean>(false);
 	const [volume, setVolume] = useState<number>(1);
+	const [currentVideo, setCurrentVideo] = useState<VideoFileWithFiles | null>(null);
 	const [videoDuration, setVideoDuration] = useState<number>(0);
 	const [currentProgress, setCurrentProgress] = useState<number>(0);
 
@@ -50,12 +51,105 @@ const VideoPlayer = ({color, videoFiles}: VideoPlayerProps) => {
 	}, []);
 
 	// Video Player Logic
+	const handleVolumeChange = (e:React.ChangeEvent<HTMLInputElement>): void => {
+		const newVolume = parseFloat(e.target.value);
+		setVolume(newVolume);
+
+		if (videoPlayer.current) {
+			videoPlayer.current.volume = newVolume;
+		}
+	};
+
+	const handleProgressChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const newProgress = parseFloat(e.target.value);
+		setCurrentProgress(newProgress);
+
+		if (videoPlayer.current) {
+			videoPlayer.current.currentTime = newProgress;
+		}
+	};
+
+	const handlePauseResume = (): void => {
+		if (!videoPlayer.current) {
+			console.log('no audio player');
+			return;
+		}
+	
+		if (isPlaying) {
+			//seekTime = audioPlayer.current.currentTime;
+			console.log('pause');
+			videoPlayer.current?.pause();
+			// may need to implement pause check (see audio player)
+			setIsPlaying(false);
+		}
+		if (!isPlaying && videoPlayer.current.readyState >= 2) {
+			//audioPlayer.current.currentTime = seekTime;
+			console.log('play');
+			videoPlayer.current?.play();
+			setIsPlaying(true);
+		}
+	};
+
+	const handleStop = (): void => {
+		if (videoPlayer.current && isPlaying) {
+			console.log('stop');
+			videoPlayer.current.pause();
+			setCurrentVideoIndex(0);
+			videoPlayer.current.currentTime = 0;
+			setIsPlaying(false);
+		}
+	};
+
+	const handleNextVideo = (): void => {
+		console.log('current video index:', currentVideoIndex);
+		setCurrentVideoIndex(
+			currentVideoIndex === videoFiles.length - 1 ? 0 : currentVideoIndex + 1
+		)
+		console.log('loading video', currentVideoIndex);
+		videoPlayer.current?.load();
+		setIsPlaying(true);
+	};
+
+	const handlePrevVideo = (): void => {
+		if (currentVideoIndex === -1) {
+			return undefined;
+		}
+		if (currentVideoIndex === 0 && videoPlayer.current) {
+			videoPlayer.current.currentTime = 0;
+			videoPlayer.current.load();
+			if (!isPlaying) setIsPlaying(true);
+		}
+		else setCurrentVideoIndex(currentVideoIndex - 1);
+	};
+
 	const handleEnded = (): void => {
 		console.log("video ended");
+		handleNextVideo();
+	};
+
+	const handleTrackSelect = (index: number) => {
+		if (index === currentVideoIndex) {
+			if (videoPlayer.current) {
+				if (isPlaying) {
+					videoPlayer.current.pause();
+					setIsPlaying(false);
+				} else {
+					videoPlayer.current.play();
+					setIsPlaying(true);
+				}
+			}
+		} else {
+			setCurrentVideoIndex(index);
+			setIsPlaying(true);
+		}
 	};
 
 	return (
-		
+		<video
+			onEnded={handleEnded}
+			ref={videoPlayer}
+			preload="metadata"
+		/>
 	);
 };
 
